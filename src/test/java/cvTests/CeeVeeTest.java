8b1b01d0ebdfb3d65c5a27adeb6ad8903b7fd734
james eisenhauer <james.eisenhauer@cambiahealth.com>
2012-04-02 13:40:42 -0700

    clean up and add comments

--- src/test/java/cvTests/CeeVeeTest.java
+++ src/test/java/cvTests/CeeVeeTest.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-
 import org.dbunit.Assertion;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
@@ -30,23 +29,27 @@ public class CeeVeeTest
 	private String testCaseName;
 	private String claimNumber;
 	
+	// data set for parameters 
+	// setting test case name and claim number
 	@SuppressWarnings("rawtypes")
 	@Parameters
     public static Collection data() {
-        return Arrays.asList(new Object[][] { { "CeeVee1", "001" }, { "CeeVee2", "002" },{ "CeeVee3", "003" }, });
+        return Arrays.asList(new Object[][] { 
+        		{ "CeeVee1", "001" }, 
+        		{ "CeeVee2", "002" },
+        		{ "CeeVee3", "003" }, });
     }
 	
 	
+	// test case constructor 
 	public CeeVeeTest(String testCaseName, String claimNumber) {
-        super();
         this.testCaseName = testCaseName;
-        this.claimNumber = claimNumber;
-        
+        this.claimNumber = claimNumber;  
     }
 	
 	
 	
-	
+	// set up the database connection
 	@Before
 	public void setUp() throws Exception {
 			
@@ -55,13 +58,14 @@ public class CeeVeeTest
 
 	}
 
+	// destroy the database connection
 	@After
 	public void tearDown() throws Exception {
 		connection.onTearDown();
 		
 	}
 
-	
+	// start the unit test
 	@Test
 	public void testClaimContents() throws Exception {
 	String compareResults = testCaseName + "_" + claimNumber + "-ERROR: ";
@@ -69,34 +73,38 @@ public class CeeVeeTest
     String claimTableName = "claims";
     String claimQuery = "select MICRO_ID, STATUS_REASON_CODE, STATUS_REASON, TOTAL_CHARGES, DEDUCTABLE, TOTAL_PATIENT_LIABILITY from claims where CLAIM_ID=";
 	
-    
+    	// Load actual claim data from database
 		ITable actualClaim = (connection.getConnection()).createQueryTable("RESULTNAME",claimQuery + claimNumber );
 		
-		// Load expected data from an XML dataset
+		// Load expected data from an XML data set
         IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File(fileSrc + testCaseName + ".xml"));
         ITable expectedClaim = expectedDataSet.getTable(claimTableName);
 
-        
+        // setting assertion handler to test doesn't fail on first assert failure
+        // this allows us to collect the failures for each line of data
         DiffCollectingFailureHandler myHandler = new DiffCollectingFailureHandler();
 
+        // surround by try catch to catch any other assert errors like incorrect data columns 
         try {
         Assertion.assertEquals(expectedClaim, actualClaim,myHandler);
         }
+        // fail test with assertion error
         catch (AssertionError ex){
         	compareResults += ex;
         	fail(compareResults);
         }
        
-        
+        // check to see if 1 or more assertion errors exist
         if (myHandler.getDiffList().size() > 0) {
         	
         	@SuppressWarnings("unchecked")
+        	// loop through assertion errors building failure message
     		List<Difference> diffList = myHandler.getDiffList();              
             for (Difference difference : diffList) {
             	compareResults += ( difference.getColumnName() + " Expected:" + difference.getExpectedValue() + " Actual:" + difference.getActualValue()  + " | ");             
             	
             } 	
-        	
+        	// fail test
         	fail(compareResults);
         	
         }
